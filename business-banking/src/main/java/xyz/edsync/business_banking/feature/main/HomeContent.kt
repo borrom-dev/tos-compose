960d37d6d45efc0aa15453e24adf9ad7eb31bd63
vuthisak <khyvuthisak@gmail.com>
2023-03-07 00:24:00 +0700

    Business-banking: added sample to test for curved

--- business-banking/src/main/java/xyz/edsync/business_banking/feature/main/HomeContent.kt
+++ business-banking/src/main/java/xyz/edsync/business_banking/feature/main/HomeContent.kt
@@ -3,17 +3,30 @@ package xyz.edsync.business_banking.feature.main
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import xyz.edsync.business_banking.R
 import xyz.edsync.business_banking.model.TabInfo
 import xyz.edsync.business_banking.ui.theme.ColorPrimary
@@ -53,7 +66,11 @@ fun HomeContent() {
                     elevation = 8.dp
                 ) {
                     Box {
-                        Box(modifier = Modifier.fillMaxWidth()) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.BottomCenter)
+                        ) {
                             TabRow(selectedTabIndex = tabState, backgroundColor = Color.White) {
                                 tabs.forEachIndexed { index, tabInfo ->
                                     Tab(selected = tabState == index, onClick = {
@@ -109,4 +126,143 @@ private fun TabItem(
             style = TextStyle(color = color)
         )
     }
+}
+
+@Composable
+private fun CustomArcShape(
+    modifier: Modifier,
+    elevation: Dp = 4.dp,
+    color: Color = ColorPrimary,
+    contentColor: Color = contentColorFor(color),
+    content: @Composable () -> Unit
+) {
+
+    val diameter = 60.dp
+    val radiusDp = diameter / 2
+
+    val cornerRadiusDp = 10.dp
+
+    val density = LocalDensity.current
+    val cutoutRadius = density.run { radiusDp.toPx() }
+    val cornerRadius = density.run { cornerRadiusDp.toPx() }
+
+    val shape = remember {
+        GenericShape { size: Size, layoutDirection: LayoutDirection ->
+//            this.roundedRectanglePath(
+//                size = size,
+//                cornerRadius = cornerRadius,
+//                fabRadius = cutoutRadius * 2
+//            )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(diameter / 2))
+
+    Box(contentAlignment = Alignment.TopCenter) {
+        FloatingActionButton(
+            shape = CircleShape,
+            contentColor = Color(0xffD32F2F),
+            modifier = Modifier
+                .offset(y = -diameter / 5)
+                .size(diameter)
+                .drawBehind {
+                    drawCircle(
+                        Color.Red.copy(.5f),
+                        radius = 1.3f * size.width / 2
+                    )
+
+                    drawCircle(
+                        Color.Red.copy(.3f),
+                        radius = 1.5f * size.width / 2
+                    )
+
+                }
+                .align(Alignment.TopCenter),
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                tint = Color.White,
+                imageVector = Icons.Filled.Close,
+                contentDescription = "Close"
+            )
+        }
+
+        Surface(
+            modifier = modifier,
+            shape = shape,
+            elevation = elevation,
+            color = color,
+            contentColor = contentColor
+        ) {
+            Column {
+                Spacer(modifier = Modifier.height(diameter))
+                content()
+
+            }
+        }
+    }
+}
+
+@Composable
+private fun CustomArcShapeSample() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        CustomArcShape(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .height(250.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Payment Failed",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Sorry !", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Your transfer to bank failed", color = Color.LightGray)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        CustomArcShape(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .height(250.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .border(1.dp, Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Payment Failed",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Sorry !", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Your transfer to bank failed", color = Color.LightGray)
+            }
+        }
+
+    }
+}
+
+@Preview()
+@Composable
+fun Test() {
+    CustomArcShapeSample()
 }
