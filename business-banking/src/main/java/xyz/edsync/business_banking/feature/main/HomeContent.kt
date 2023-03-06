8e56d66a376005706336f73fbd60d2731ecd5d00
vuthisak <khyvuthisak@gmail.com>
2023-03-06 23:44:45 +0700

    Business-banking: updated tab layout home screen

--- business-banking/src/main/java/xyz/edsync/business_banking/feature/main/HomeContent.kt
+++ business-banking/src/main/java/xyz/edsync/business_banking/feature/main/HomeContent.kt
@@ -2,16 +2,21 @@ package xyz.edsync.business_banking.feature.main
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import xyz.edsync.business_banking.R
 import xyz.edsync.business_banking.model.TabInfo
+import xyz.edsync.business_banking.ui.theme.ColorPrimary
 import xyz.edsync.business_banking.ui.theme.ColorSecondaryText
 import xyz.edsync.common.util.ui.DefaultText
 import xyz.edsync.toscompose.theme.TosComposeTheme
@@ -21,19 +26,58 @@ fun HomeContent() {
     val tabs = mutableListOf(
         TabInfo(R.string.text_home, R.drawable.ic_banking_home),
         TabInfo(R.string.text_expenses, R.drawable.ic_banking_expenses),
+        TabInfo(-1, -1),
         TabInfo(R.string.text_wallet, R.drawable.ic_banking_wallet),
         TabInfo(R.string.text_profile, R.drawable.ic_banking_profile)
     )
+    var tabState by remember { mutableStateOf(0) }
     TosComposeTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
         ) {
-            Column {
-                TabRow(selectedTabIndex = 0) {
-                    Tab(selected = true, onClick = {
-                    }) {
-                        tabs.forEach { TabItem(IconRes = it.iconRes, titleRes = it.titleRes) }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .background(color = Color.White)
+                )
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    backgroundColor = Color.White,
+                    shape = RoundedCornerShape(0.dp),
+                    elevation = 8.dp
+                ) {
+                    Box {
+                        Box(modifier = Modifier.fillMaxWidth()) {
+                            TabRow(selectedTabIndex = tabState, backgroundColor = Color.White) {
+                                tabs.forEachIndexed { index, tabInfo ->
+                                    Tab(selected = tabState == index, onClick = {
+                                    }) {
+                                        if (tabInfo.iconRes != -1) {
+                                            TabItem(
+                                                IconRes = tabInfo.iconRes,
+                                                titleRes = tabInfo.titleRes,
+                                                tabState == index
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        FloatingActionButton(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .padding(bottom = 16.dp),
+                            onClick = { },
+                            backgroundColor = ColorPrimary
+                        ) {
+                            DefaultText(text = "+")
+                        }
                     }
                 }
             }
@@ -44,17 +88,25 @@ fun HomeContent() {
 @Composable
 private fun TabItem(
     @DrawableRes IconRes: Int,
-    @StringRes titleRes: Int
+    @StringRes titleRes: Int,
+    isSelected: Boolean = false
 ) {
-    Column {
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         val title = stringResource(id = titleRes)
+        val color = if (isSelected) ColorPrimary else ColorSecondaryText
         Icon(
             painter = painterResource(id = IconRes),
-            contentDescription = title
+            contentDescription = title,
+            tint = color
         )
         DefaultText(
             text = stringResource(id = titleRes),
-            style = TextStyle(color = ColorSecondaryText)
+            style = TextStyle(color = color)
         )
     }
 }
