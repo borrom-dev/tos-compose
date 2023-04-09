620dfc0c455921e0441bc93698a4fe98e046d16c
vuthisak <khyvuthisak@gmail.com>
2023-04-09 12:29:32 +0700

    Business-banking: update send money to

--- business-banking/src/main/java/xyz/edsync/business_banking/feature/home/wallet/WalletContent.kt
+++ business-banking/src/main/java/xyz/edsync/business_banking/feature/home/wallet/WalletContent.kt
@@ -3,18 +3,22 @@ package xyz.edsync.business_banking.feature.home.wallet
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -53,19 +57,102 @@ private fun Content() {
         stringResource(id = R.string.text_month),
         stringResource(id = R.string.text_year)
     )
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
+    LazyColumn {
+        item {
+            SendMoneyToContent()
+            Spacer(modifier = Modifier.size(16.dp))
+            DefaultText(
+                text = stringResource(id = R.string.title_transactions),
+                fontSize = 20.sp,
+                style = TextStyle(color = ColorDarkPrimary, fontWeight = FontWeight.SemiBold)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            TransactionFilterHeader(currentPage, items)
+        }
+        items(5) {
+            TransactionItem()
+        }
+    }
+}
+
+@Composable
+private fun SendMoneyToContent() {
+    DefaultText(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        text = stringResource(id = R.string.title_send_money_to),
+        fontSize = 20.sp,
+        style = TextStyle(color = ColorDarkPrimary, fontWeight = FontWeight.SemiBold)
+    )
+    Spacer(modifier = Modifier.size(8.dp))
+    LazyRow(modifier = Modifier.fillMaxWidth()) {
+        items(count = 5) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Spacer(modifier = Modifier.size(16.dp))
+                Card(
+                    modifier = Modifier
+                        .width(90.dp)
+                        .height(110.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    backgroundColor = Color.White
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_add_new_contract),
+                            contentDescription = "Add new contract"
+                        )
+                        Spacer(modifier = Modifier.size(8.dp))
+                        DefaultText(
+                            text = "Add new contact",
+                            style = TextStyle(
+                                color = ColorSecondaryText,
+                                fontSize = 13.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TransactionItem() {
+    Card(
+        modifier = Modifier.padding(16.dp),
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Spacer(modifier = Modifier.size(16.dp))
-        DefaultText(
-            text = stringResource(id = R.string.title_transactions),
-            fontSize = 20.sp,
-            style = TextStyle(color = ColorDarkPrimary, fontWeight = FontWeight.SemiBold)
-        )
-        Spacer(modifier = Modifier.size(8.dp))
-        TransactionFilterHeader(currentPage, items)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.size(40.dp),
+                painter = painterResource(id = R.drawable.ic_gas),
+                contentDescription = "gas"
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // TODO to update string
+                Column(horizontalAlignment = Alignment.Start) {
+                    DefaultText(text = "Shell", style = TextStyle(color = Color.Black))
+                    DefaultText(text = "17 Monday June", style = TextStyle(color = Color.Black))
+                }
+                DefaultText(text = "-$35,88", style = TextStyle(color = Color.Red))
+            }
+        }
     }
 }
 
