ea9e359da9588c4a241f1f22cdb25535a4b8a9b2
vuthisak <khyvuthisak@gmail.com>
2023-06-04 10:43:53 +0700

    Features/slide-transfom: update menus

--- slide-transfom/src/main/java/xyz/edsync/slidetransfom/SlideTransformContent.kt
+++ slide-transfom/src/main/java/xyz/edsync/slidetransfom/SlideTransformContent.kt
@@ -1,8 +1,6 @@
 package xyz.edsync.slidetransfom
 
-import android.content.Context
 import androidx.annotation.StringRes
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -11,14 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -29,46 +26,52 @@ import xyz.edsync.common.util.theme.Orange
 import xyz.edsync.common.util.ui.DefaultText
 import xyz.edsync.slidetransfom.ui.theme.TosComposeTheme
 
+private val menus = Menu(
+    title = 0,
+    sampleNames = mutableListOf<Int>().apply {
+        add(R.string.slide_stack)
+    }
+)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun MainContent(listener: ItemClickListener<Int>) {
+internal fun SlideTransformContent(listener: ItemClickListener<Int>) {
     TosComposeTheme {
-        val context: Context = LocalContext.current
-        Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
 
-        }, floatingActionButton = {
+            }, floatingActionButton = {
 
-        }) {
-            Body(paddingValues = it, menus = arrayListOf(), listener = listener)
+            }) {
+            Body(paddingValues = it, menu = menus, listener = listener)
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun Body(
     paddingValues: PaddingValues,
-    menus: MutableList<Menu>,
+    menu: Menu,
     listener: ItemClickListener<Int>
 ) {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        menus.forEach { item ->
-            itemsIndexed(item.sampleNames) { index: Int, sampleName: Int ->
-                BodyItem(
-                    sampleName = sampleName,
-                    isLastItem = index == item.sampleNames.size - 1,
-                    listener
-                )
-            }
+        items(items = menu.sampleNames) { index ->
+            BodyItem(
+                sampleName = menu.sampleNames[index],
+                isLastItem = index == menu.sampleNames.size - 1,
+                listener
+            )
         }
     }
 }
 
 @Composable
 private fun BodyItem(
-    @StringRes sampleName: Int, isLastItem: Boolean = false, listener: ItemClickListener<Int>
+    @StringRes sampleName: Int,
+    isLastItem: Boolean = false,
+    listener: ItemClickListener<Int>
 ) {
     Column(modifier = Modifier
         .fillMaxWidth()
