c09b40fe309ba29730eb7fba6cdb5ef613a1e960
vuthisak <khyvuthisak@gmail.com>
2023-08-03 00:46:53 +0700

    Updated lines indicator

---
+++ quiz-game/src/main/java/xyz/edsync/quiz_game/feature/ui/component.kt
@@ -0,0 +1,23 @@
+package xyz.edsync.quiz_game.feature.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun SmallButton(
+    modifier: Modifier = Modifier,
+    painter: Painter,
+    onClick: () -> Unit
+) {
+
+    Button(
+        modifier = Modifier,
+        onClick = onClick
+    ) {
+        Image(painter = painter, contentDescription = "ButtonImage")
+    }
+
+}
