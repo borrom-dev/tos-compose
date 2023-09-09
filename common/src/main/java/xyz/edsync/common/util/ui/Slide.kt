69944db8d3aee24c6f345a5ecb5b4c3719e445c8
vuthisak <khyvuthisak@gmail.com>
2023-09-10 00:53:25 +0700

    features: add infinite slide

---
+++ common/src/main/java/xyz/edsync/common/util/ui/Slide.kt
@@ -0,0 +1,80 @@
+package xyz.edsync.common.util.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+fun ImageSlider(images: List<Any>) {
+    var currentImageIndex by remember { mutableStateOf(0) }
+    var isAnimating by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(100.dp)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // Scrollable Row of Cards
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                itemsIndexed(images) { index, image ->
+                    Card(
+                        modifier = Modifier
+                            .width(300.dp)
+                            .height(200.dp)
+                            .clickable {
+                                if (index != currentImageIndex && !isAnimating) {
+                                    isAnimating = true
+                                    coroutineScope.launch {
+                                        val delayMillis = 500L
+                                        // Wait for the card to change color before animating
+                                        delay(delayMillis / 2)
+                                        currentImageIndex = index
+                                        delay(delayMillis)
+                                        isAnimating = false
+                                    }
+                                }
+                            },
+                        elevation = CardDefaults.elevatedCardElevation(4.dp)
+                    ) {
+//                        NetworkImage(
+//                            contentDescription = "",
+//                            url = image as String,
+//                            width = 300,
+//                            height = 200
+//                        )
+                    }
+                }
+
+            }
+
+        }
+    }
+}
